ecc091102c68c79344e869e492cdbab030ad62eb
Tommaso Pagliani <tp060202@gmail.com>
2020-04-28 21:15:44 +0200

    troppo da spiegare

--- scripts.js
+++ scripts.js
@@ -20,8 +20,34 @@ const Aggiungi = function (name, costo) {
 
     let element = document.getElementById("carrello")
     element.appendChild(row)
+
+    final.cart.push(name)
+    final.cost += costo
 }
 
-const cart = {
+const final = {
+    cart : [],
+    cost: 0
+}
+
+const SEND  = function (){
+    final.Email = document.getElementById("Email").value
+    final.Nome = document.getElementById("Nome").value
+    final.Cognome = document.getElementById("Cognome").value
+    final.Indirizzo = document.getElementById("Indirizzo").value
+    final.Comune = document.getElementById("Comune").value
+    final.Sede = document.getElementById("Sede").value
+    final.Sezione = document.getElementById("Sezione").value
+
     
+    let jsonData = JSON.stringify(final)
+
+    function download(content, fileName, contentType) {
+        var a = document.createElement("a");
+        var file = new Blob([content], {type: contentType});
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+    }
+    download(jsonData, 'json.txt', 'text/plain');
 }
