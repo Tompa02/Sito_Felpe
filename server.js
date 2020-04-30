0a565e4d850608b234ff930376a3fddf281ebdef
Tommaso Pagliani <tp060202@gmail.com>
2020-04-30 13:24:32 +0200

    100%

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ const express = require('express')
 const path = require('path')
 const crypto = require('crypto')
 const parser = require('body-parser')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const server = express()
+
+const csvWriter = createCsvWriter({
+    path: './ordini.csv',
+    header: [
+        {id: 'nome', title: 'NOME'},
+        {id: 'cognome', title: 'COGNOME'},
+        {id: 'email', title: 'EMAIL'},
+        {id: 'indirizzo', title: 'INDIRIZZO'},
+        {id: 'comune', title: 'COMUNE'},
+        {id: 'cap', title: 'CAP'},
+        {id: 'sede', title: 'SEDE'},
+        {id: 'sezione', title: 'SEZIONE'},
+        {id: 'classe', title: 'CLASSE'},
+        {id: 'id', title: 'ID'},
+        {id: 'importo', title: 'IMPORTO'},
+        {id: 'carrello', title: 'CARRELLO'}
+    ]
+});
 
 const prezzi = {
     'Felpa Tradizionale': 20,
@@ -49,6 +68,27 @@ const verify = function(req, res, next) {
 server.post('/register_order', verify, (req, res) => {
     const cost = calcola_spesa(req.body.cart)
     const id = crypto.createHash('md5').update(JSON.stringify(req.body)).digest("hex")
+    const check = id
+    const appends =[{
+        nome:req.body.Nome,
+        cognome:req.body.Cognome,
+        email:req.body.Email,
+        indirizzo:req.body.Indirizzo,
+        comune:req.body.Comune,
+        cap:req.body.CAP,
+        sede:req.body.Sede,
+        sezione:req.body.Sezione,
+        classe:req.body.Classe,
+        id:check,
+        importo:cost,
+        carrello:req.body.cart
+    }]
+
+    csvWriter.writeRecords(appends)
+    .then(() => {
+        console.log('...Done');
+    });
+
     res.send({"status": 200, "cost": cost, "id": id})
 })
 
