0b4a4899ebe43adf673f943d47ea70211292a7e0
fedepesso <fpessina2002@gmail.com>
2020-05-17 22:27:51 +0200

    adesso vengono inviate mails singole

--- send_confirmation.js
+++ send_confirmation.js
@@ -8,7 +8,7 @@ const chain_mails = function() {
     let ordini_cancellati = fs.readFileSync('ordini_rimossi.csv', 'utf8').split('\n').slice(1)
     mail_array = ordini.filter(line=>!ordini_cancellati.includes(line[0])).map(line=>line[3]) // prendo tutte le mail associate ad ordini il cui id non appare in ordini_rimossi.csv
     console.log(mail_array)
-    return mail_array.join(', ')
+    return mail_array
 }
 
 const send_confirmation_request = function () {
@@ -16,25 +16,26 @@ const send_confirmation_request = function () {
     if (!mails) {
         return 0
     }
-    
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
           user: 'felpe.las@gmail.com',
           pass: 'ALPHAbetaCharlie30'
         }
-    });
+    })
 
-    const mailOptions = {
-        from: 'felpe.las@gmail.com',
-        to: mails,
-        subject: "Richiesta di conferma ordine",
-        html : ejs.render(fs.readFileSync(__dirname + "/public/redirect.html", 'utf-8'))
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-        console.log(error)
-    });
+    mails.forEach(val => {
+        const mailOptions = {
+            from: 'felpe.las@gmail.com',
+            to: val,
+            subject: "Richiesta di conferma ordine",
+            html : ejs.render(fs.readFileSync(__dirname + "/public/redirect.html", 'utf-8'))
+        };
+    
+        transporter.sendMail(mailOptions, function(error, info){
+            console.log(error, info)
+        })
+    })
 }
 
 send_confirmation_request()
